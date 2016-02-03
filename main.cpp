44a0738e09253855d83af7c24a2a2b8f92836ed5
Keanu Interone <interonekt@comcast.net>
2016-02-03 10:15:05 -0700

    update main and make

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#incldue <iostream>
+#include <iostream>
 
 int main() {
     std::cout << "its a bitch girl" << std::endl;
+    std::cout << "will this make a new executable" << std::endl;
     return 0;
 }
